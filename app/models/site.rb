323ac4ee159d6e165d1535e0a53fb6ea053499dc
Kenneth <kennethkapundi1@gmail.com>
2014-09-11 16:22:57 +0200

    updated application home page

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,23 @@
 class Site < ActiveRecord::Base
   set_primary_key :site_id
 
+  def self.alerts
+    alerts = {"urgent" => [],
+              "average" => [],
+              "normal" => []
+    }
+    Observation.sorted_sites_failures.each do |site, errors|
+
+
+       if errors > 100
+         alerts["urgent"] << site
+       elsif errors > 0
+          alerts["average"] << site
+       else
+         alerts["normal"] << site
+       end
+    end
+
+    return alerts
+  end
 end
